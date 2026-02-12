5b8c7d1c8c0cebcde93bf521018fa506f57b5aa1
Ryan Saturno <ryanzig56@gmail.com>
2026-02-12 17:00:21 -0300

    Primeiro commit: estruturação do projeto

---
+++ src/test/java/zignanimotors/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package zignanimotors.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
